1793b373c10e00542b51a720ddba866902385c26
Yaroslava Terletska <yaroslava1408@ukr.net>
2023-05-15 23:28:32 +0200

    Add cart

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -28,32 +29,66 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
   };
-
+  
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active',
+    },
+    // CODE ADDED END
   };
-
+  
   const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
+    },
+    // CODE ADDED END
   };
-
+  
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
   };
 
+  
   class Product {
 
     constructor (id, data){
@@ -234,6 +269,7 @@
       thisProduct.amountWidget = new AmountWidget(thisProduct.AmountWidgetElem);
       thisProduct.AmountWidgetElem.addEventListener ('updated', function () {
         thisProduct.processOrder();
+     
       });
       
     }
@@ -251,9 +287,10 @@
       thisWidget.getElements(element);
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
-      
-      
+         
     }
+
+    
    
     getElements(element) {
       const thisWidget = this;
@@ -274,16 +311,20 @@
               
 
       /* TODO: Add validation */
-      if (thisWidget.value !== newValue && !isNaN(newValue)
-        && newValue <= settings.amountWidget.defaultMax && newValue >= settings.amountWidget.defaultMin) {
+      if (thisWidget.value != newValue && !isNaN(newValue)) {
+        //&& newValue <= settings.amountWidget.defaultMax && newValue >= settings.amountWidget.defaultMin) {
         thisWidget.value = newValue;      
         console.log ('new written value', newValue);
-        thisWidget.announce();       
-      }
-              
+        thisWidget.announce();     
+       
+      }   
+
+      // thisWidget.AmountWidgetElem.innerHTML = newValue;
+  
     }
-     
     
+    
+
     initActions () {
       const thisWidget = this;
 
@@ -303,10 +344,9 @@
         event.preventDefault();
         thisWidget.setValue(thisWidget.value  + 1 );
         console.log ('increase', thisWidget.value  + 1);
-        
+              
       });
-     
-        
+              
     }
 
     announce() {
@@ -314,6 +354,7 @@
   
       const event = new Event('updated');
       thisWidget.element.dispatchEvent(event);
+        
     }
   } 
   
